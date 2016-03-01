af3e6602e93c948aedf6d6ae7d1d375bd0a7330b
rohit <rkugver1@binghamton.edu>
2016-03-01 14:40:31 -0500

    template char driver

---
+++ .pipe_drv.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/rohit/workspace/pipe_driver/pipe_drv.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/rohit/workspace/pipe_driver/pipe_drv.ko /home/rohit/workspace/pipe_driver/pipe_drv.o /home/rohit/workspace/pipe_driver/pipe_drv.mod.o
---
+++ .tmp_versions/pipe_drv.mod
@@ -0,0 +1,2 @@
+/home/rohit/workspace/pipe_driver/pipe_drv.ko
+/home/rohit/workspace/pipe_driver/pipe_drv.o
