8ef2d8f62bf13fda10a210739ace2aeb851629f8
rohit <rkugver1@binghamton.edu>
2016-03-15 17:42:44 -0400

    write hangs

--- pipe_drv.c
+++ pipe_drv.c
@@ -17,16 +17,48 @@
  */
 
 #include <linux/module.h>
-
+#include<linux/string.h>
 #include <linux/sched.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
-
+#include <linux/slab.h>
 #include <asm/segment.h>
+#include <asm/uaccess.h>
+#define MAX_Q_SIZE 5
 
 static struct miscdevice pipe_dev;
+static DECLARE_WAIT_QUEUE_HEAD(pro_wq);
+static DECLARE_WAIT_QUEUE_HEAD(con_wq);
+static DEFINE_MUTEX(mtx);
+
+static int flag = 0;
+
+int size = MAX_Q_SIZE; 
+struct queue { 
+    int size;      //Maximum slots in buffer. Can be passed from cmd line during insmod
+    int r;      //read ptr
+    int w;      //write ptr
+    char **arr;  //array of pointers to strings
+};
+
+struct queue q;
+
+int init_queue( void )
+{
+   q.size = size;
+   q.arr =  kmalloc(sizeof( char* )* q.size, GFP_KERNEL);
+   if(q.arr== NULL){
+       return -1;
+   }
+   memset(q.arr, 0, sizeof( char* )* q.size);
+   q.r = 0;   //read index
+   q.w = 0;   //write index
+   return 0;
+}
 
 
+module_param(size, int, 0);
+MODULE_PARM_DESC(q_size, "number of slots in pipe");
 /* =================================== a simple delayer */
 
 static void delay_tenths(int tenths)
@@ -39,9 +71,7 @@ static void delay_tenths(int tenths)
         schedule();
 }
 
-/* =================================== open and close */
 
-//int pipe_open (struct inode *inode, struct file *filp)
 int pipe_open (struct inode *inode, struct file *file)
 {
     //MOD_INC_USE_COUNT;
@@ -55,35 +85,52 @@ void pipe_close (struct inode *inode, struct file *filp)
     printk(KERN_ALERT "pipe_drv: close called %s %d \n",__FUNCTION__,__LINE__);
 }
 
-/* =================================== read and write */
 
-static DECLARE_WAIT_QUEUE_HEAD(wq);
-static int flag = 0;
-/*
- * The read() method just returns the current status of the leds.
- * Try "while true; do dd bs=1 if=/dev/kiss count=1; sleep 1; done"
- */
 ssize_t pipe_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
     printk(KERN_DEBUG "process %i (%s) going to sleep\n",
             current->pid, current->comm);
-    wait_event_interruptible(wq, count > 0);
-    count--;
-    printk(KERN_DEBUG "awoken %i (%s)\n", current->pid, current->comm);
+    //wait_event_interruptible(wq, count > 0);
+    //count--;
+    //printk(KERN_DEBUG "awoken %i (%s)\n", current->pid, current->comm);
     return 0; /* EOF */
 }
 
-/*
- * Write understands a few textual commands
- */ 
 ssize_t pipe_write (struct file *filp, const char __user *buf, size_t count,
                         loff_t *f_pos)
 {
-    printk(KERN_DEBUG "process %i (%s) awakening the readers...\n",
+    //get the lock
+    if ( mutex_lock_interruptible(&mtx)){
+        return -EINTR;
+    }
+    while( q.r == q.r ) {
+        printk(KERN_DEBUG "process%i (%s): q full, sleeping.\n", current->pid, current->comm);
+        mutex_unlock(&mtx);
+        wait_event_interruptible(pro_wq, q.r != q.w);
+        if ( mutex_lock_interruptible(&mtx)){
+            return -EINTR;
+        }
+
+    }
+   //we have the lock and slot available to write
+   q.arr[q.w] =  kmalloc(sizeof( char* )* q.size, GFP_KERNEL);
+   if(q.arr[q.w] == NULL){
+       //!!!!!!!! release lock here
+       return -1;
+   }
+   if (copy_from_user(q.arr[q.w], buf, count)) {
+       //up (&dev->sem);
+       //!!!!!!!! release lock here
+       return -EFAULT;
+   }
+   printk(KERN_DEBUG "process%i (%s): copied to slot %d\n", current->pid, current->comm, q.w);
+   q.w = (q.w + 1) % q.size;   //more write head
+   mutex_unlock(&mtx);  // releas the lock and then wakeup others.
+                        // Otherwise woken up processes cant get theis lock
+   printk(KERN_DEBUG "process %i (%s) awakening the readers...\n",
             current->pid, current->comm);
-    count++;
-    wake_up_interruptible(&wq);
-    return count; /* succeed, to avoid retrial */
+   wake_up_interruptible(&con_wq);
+   return count; /* succeed, to avoid retrial */
 
 }
 
@@ -101,39 +148,28 @@ struct file_operations pipe_fops = {
 //	.release =	scull_p_release,
 //	.fasync =	scull_p_fasync,
 };
-#if 0
-/*
-struct file_operations pipe_fops = {
-    NULL,
-    pipe_read,
-    pipe_write,
-    NULL,          /* pipe_readdir */
-    NULL,           /* pipe_select */
-    NULL,          /* pipe_ioctl */
-    NULL,          /* pipe_mmap */
-    pipe_open,
-    pipe_close,
-    NULL,          /* pipe_fsync */
-    NULL,          /* pipe_fasync */
-                   /* nothing more, fill with NULLs */
-    NULL,
-};
-#endif
-
 /* =================================== init and cleanup */
 
 int init_module(void)
 {
     int retval;
     pipe_dev.minor = MISC_DYNAMIC_MINOR;
-    pipe_dev.name = "pipe-file";    //This will be the name od the /dev/ file. We may need to take it as input arguement for insmod
+    pipe_dev.name = "pipe-file";    //This will be the name od the /dev/ file. 
+                                    //We may need to take it as input arguement for insmod
     pipe_dev.fops = &pipe_fops;
     retval = misc_register(&pipe_dev);
-    printk(KERN_ALERT "pipe_drv: init called %s %d \n",__FUNCTION__,__LINE__);
+    printk(KERN_ALERT "pipe_drv: init called. size = %d.  %s %d \n", size, __FUNCTION__,__LINE__);
     if (retval) {
         printk(KERN_ALERT "pipe_drv: register error %s %d \n",__FUNCTION__,__LINE__);
         return retval;
     }
+    if( init_queue( ) < 0)  {
+        printk(KERN_ALERT "pipe_drv: init_queue error %s %d \n",__FUNCTION__,__LINE__);
+        return -1;
+    }
+    mutex_init(&mtx);
+    init_waitqueue_head(&pro_wq);
+    //init_waitqueue_head(&con_wq);
     return 0;
 }
 
